21d494a391b3fe56c20ff6f452ae4487dd7ad44d
João Santos <jps@xce.pt>
2021-10-25 19:47:17 +0100

    Made error reporting more accessible by automatically focusing and selecting the error message for copying to the clipboard.

--- Error.lua
+++ Error.lua
@@ -1,28 +1,25 @@
-local function Search(frame, type)
-    if frame:IsObjectType(type) then
-        return frame
-    end
-    for index, frame in ipairs({frame:GetChildren()}) do
-        local result = Search(frame, type)
-        if result then
-            return result
-        end
-    end
-    return nil
-end
-
 local function Read()
-    local errorFrame = Search(ScriptErrorsFrame, "EditBox")
-    if not errorFrame then
+    if not ScriptErrorsFrame:IsShown() then
+        Vimp_Say("No errors!")
         return
     end
+    local errorFrame = ScriptErrorsFrame:GetEditBox()
     Vimp_Shut()
     Vimp_Say(errorFrame:GetDisplayText())
+    errorFrame:SetFocus()
+    errorFrame:HighlightText()
 end
 
 local function OnShow(frame)
-    Vimp_Say("Lua errors")
+    Vimp_Say("Lua error")
+end
+
+local function OnEvent(frame, event)
+    if event == "LUA_WARNING" then
+        UIParent:UnregisterEvent("LUA_WARNING")
+    end
 end
 
 Vimp_AddCommand("error", Read)
 ScriptErrorsFrame:HookScript("OnShow", OnShow)
+UIParent:HookScript("onEvent", OnEvent)
